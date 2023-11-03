6c84be0a316bb942b1b59aff0626d52e233e1dbe
Lugard Agu <agulugard@gmail.com>
2023-11-03 12:07:38 +0100

    Update style.css and app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// const API_KEY = 'a1405ed4af4bc83f1e312cd53d0a8854';
+
+// API Read access token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTQwNWVkNGFmNGJjODNmMWUzMTJjZDUzZDBhODg1NCIsInN1YiI6IjY1NDQyNzAzOGM3YjBmMDBhZDE3OWE4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BDOuAwY1V4m1RbE1kmJHU0u7hRQRYYrLgtIqzNJWs-w'
+
+// grab different components sections
+
+const input = document.querySelector('.search input');
+const btn = document.querySelector('.search button');
+const main_grid_title = document.querySelector('.favorites h1');
+const main_grid = document.querySelector('.favorites .movies-grid');
+
+// btn.addEventListener('click', add_searched_movies_to_dom)
+async function get_movie_by_search () {
+  
+}
