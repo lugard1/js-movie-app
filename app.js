f668f3da6b78bad24beac511067a4a20e21691bd
Lugard Agu <agulugard@gmail.com>
2023-11-07 23:51:54 +0100

    Add key

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const search_term = 'a1405ed4af4bc83f1e312cd53d0a8854';
-// const api_key = search_term;
+const api_key = search_term;
 
 // API Read access token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTQwNWVkNGFmNGJjODNmMWUzMTJjZDUzZDBhODg1NCIsInN1YiI6IjY1NDQyNzAzOGM3YjBmMDBhZDE3OWE4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BDOuAwY1V4m1RbE1kmJHU0u7hRQRYYrLgtIqzNJWs-w'
 
@@ -13,7 +13,7 @@ const main_grid = document.querySelector('.favorites .movies-grid');
 // btn.addEventListener('click', add_searched_movies_to_dom)
 
 async function get_movie_by_search (search_term) {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/157336?api_key=${search_term}&append_to_response=videos,images`);
+  const response = await fetch(`https://api.themoviedb.org/3/movie/157336?api_key=${api_key}&append_to_response=videos,images`);
   const resData = await response.json();
   return resData;
 }
@@ -24,7 +24,7 @@ btn.addEventListener('click', add_searched_movies_to_dom);
 
 async function add_searched_movies_to_dom () {
   const data = await get_movie_by_search(input.value);
-  // console.log(data);
+  console.log(data);
 }
   
 add_searched_movies_to_dom ();
