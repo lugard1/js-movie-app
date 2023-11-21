de90818571b4ea4f42cd3f541e83f9c6171a84c3
Lugard Agu <agulugard@gmail.com>
2023-11-21 10:00:46 +0100

    Implement popup close action

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const btn = document.querySelector('.search .btn');
 const moviesGrid = document.querySelector('.favorites .movies-grid');
 
 const popup_container = document.querySelector('.popup-container');
-// const close = document.querySelector('.x-icon')
+const close = document.querySelector('.x-icon');
+
 
 function add_click_effect_to_card(cards) {
   cards.forEach(card => {
-    card.addEventListener('click', () => show_popup(card))
-  })
+    card.addEventListener('click', () => show_popup(card));
+  });
 }
 
 // Search Movies
@@ -28,6 +29,8 @@ function displayMovies(movies) {
     const card = document.createElement('div');
     card.classList.add('card');
     card.dataset.id = movie.id;
+    // card.dataset.trailerKey = movie.trailerKey;
+    // card.dataset.overview = movie.overview;
 
     card.innerHTML = `
       <div class="img">
@@ -56,30 +59,31 @@ function displayMovies(movies) {
 btn.addEventListener('click', searchMovies);
 
 //Popup
-async function searchMovies_by_id(id) {
+async function searchMovies_by_id() {
   // const query = input.value;
   const response = await fetch(`https://api.themoviedb.org/3/search/movie/${id}?api_key=${API_KEY}`);
   const resData = await response.json();
-  console.log(resData);
+  // console.log(resData);
 
-  // return resData.results;
+  return resData.results;
 }
 
-searchMovies_by_id(id);
+searchMovies_by_id();
 
 function show_popup(card) {
-  // console.log('Popup is shown' + card)
+  const movieId = card.dataset.id;
+  const imageURL = card.querySelector('.img img').src;
+  const trailerKey = card.dataset.trailerKey;
+
+  // Update the content of the popup with the relevant information
+  popup_container.querySelector('.poster-img img').src = imageURL;
+  popup_container.querySelector('.trailer iframe').src = `https://www.youtube.com/embed/${trailerKey}`;
+
+  // Show the popup
   popup_container.classList.add('show-popup')
 }
 
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('x-icon')) {
-    popup_container.classList.remove('show-popup');
-  }
+close.addEventListener('click', () => {
+  // console.log('Close button clicked');
+  popup_container.classList.remove('show-popup');
 });
-
-
-// close.addEventListener('click', () => {
-//   console.log('Close button clicked');
-//   popup_container.classList.remove('show-popup');
-// });
