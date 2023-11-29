b6b6ba0215327852e61603f1ef1d7b719134db5a
Lugard Agu <agulugard@gmail.com>
2023-11-29 01:00:48 +0100

    Update index.html and app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+// Your API_KEY should be defined in config.js
+
 const input = document.querySelector('.search input');
 const btn = document.querySelector('.search .btn');
 const moviesGrid = document.querySelector('.favorites .movies-grid');
 
-const popup_container = document.querySelector('.popup-container');
+const popupContainer = document.querySelector('.popup-container');
 const close = document.querySelector('.x-icon');
 
-
-function add_click_effect_to_card(cards) {
+function addClickEffectToCard(cards) {
   cards.forEach(card => {
-    card.addEventListener('click', () => show_popup(card));
+    card.addEventListener('click', () => showPopup(card));
   });
 }
 
@@ -29,8 +30,8 @@ function displayMovies(movies) {
     const card = document.createElement('div');
     card.classList.add('card');
     card.dataset.id = movie.id;
-    // card.dataset.trailerKey = movie.trailerKey;
-    // card.dataset.overview = movie.overview;
+    card.dataset.trailerKey = '';
+    card.dataset.overview = movie.overview;
 
     card.innerHTML = `
       <div class="img">
@@ -52,49 +53,77 @@ function displayMovies(movies) {
     moviesGrid.appendChild(card);
   });
 
-  const cards = document.querySelectorAll('.card')
-  add_click_effect_to_card(cards)
+  const cards = document.querySelectorAll('.card');
+  addClickEffectToCard(cards);
 }
 
-btn.addEventListener('click', searchMovies);
+btn.addEventListener('click', () => searchMovies());
 
-//Popup
-async function searchMovies_by_id() {
-  // const query = input.value;
-  const response = await fetch(`https://api.themoviedb.org/3/search/movie/${id}?api_key=${API_KEY}`);
+// Popup
+async function searchMoviesById(id) {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`);
+  const movieDetails = await response.json();
+  return movieDetails;
+}
+
+async function searchMoviesTrailer(id) {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`);
   const resData = await response.json();
-  // console.log(resData);
 
-  return resData;
+  if (resData.results.length > 0) {
+    return resData.results[0].key;
+  } else {
+    return null;
+  }
 }
 
-searchMovies_by_id();
-
-async function searchMovies_trailer(id) {
-  // const query = input.value;
-  const response = await fetch(`https://api.themoviedb.org/3/search/movie/${id}/videos?api_key=${API_KEY}`);
-  const resData = await response.json();
-  // console.log(resData);
-
-  return resData.results;
-}
-
-searchMovies_trailer()
-
-function show_popup(card) {
+function showPopup(card) {
   const movieId = card.dataset.id;
   const imageURL = card.querySelector('.img img').src;
-  const trailerKey = card.dataset.trailerKey;
 
-  // Update the content of the popup with the relevant information
-  popup_container.querySelector('.poster-img img').src = imageURL;
-  popup_container.querySelector('.trailer iframe').src = `https://www.youtube.com/embed/${trailerKey}`;
+  searchMoviesById(movieId)
+    .then(movieDetails => {
+      if (!movieDetails || movieDetails.title === null || movieDetails.overview === null) {
+        console.error('Error: Movie details not found or incomplete.');
+        return Promise.reject('Movie details not found or incomplete.');
+      }
 
-  // Show the popup
-  popup_container.classList.add('show-popup')
+      document.getElementById('popupPoster').src = imageURL;
+      document.querySelector('.movie-title').textContent = movieDetails.title || 'Title not available';
+      document.getElementById('popupLanguage').textContent = 'English'; // Replace with actual language data
+      document.getElementById('popupLength').textContent = '120 minutes'; // Replace with actual length data
+      document.getElementById('popupRate').textContent = `${movieDetails.vote_average}/10`;
+      document.getElementById('popupBudget').textContent = `$1350000`; // Replace with actual budget data
+      document.getElementById('popupReleaseDate').textContent = '05-07-2022'; // Replace with actual release date data
+
+      // Genres
+      const genresList = document.getElementById('popupGenres');
+      genresList.innerHTML = '';
+      movieDetails.genres.forEach(genre => {
+        const li = document.createElement('li');
+        li.textContent = genre.name;
+        genresList.appendChild(li);
+      });
+
+      document.getElementById('popupOverview').textContent = movieDetails.overview || 'Overview not available';
+
+      return searchMoviesTrailer(movieId);
+    })
+    .then(trailerKey => {
+      const trailerIframe = document.querySelector('.trailer iframe');
+      if (trailerKey) {
+        trailerIframe.src = `https://www.youtube.com/embed/${trailerKey}`;
+      } else {
+        trailerIframe.src = '';
+      }
+
+      popupContainer.classList.add('show-popup');
+    })
+    .catch(error => {
+      console.error('Error in showPopup:', error);
+    });
 }
 
 close.addEventListener('click', () => {
-  // console.log('Close button clicked');
-  popup_container.classList.remove('show-popup');
+  popupContainer.classList.remove('show-popup');
 });
